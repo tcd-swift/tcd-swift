b143987edbb39b4d00cc17ffcf8c9c49dfad4149
Shayan Yousefizadeh <yousefs@tcd.ie>
2014-12-24 00:52:20 +0000

    Left out multiplication action code...

--- src/test/Test.swift
+++ src/test/Test.swift
@@ -1,1 +1,3 @@
 var x: Int = 420
+var y: Int = 400 + 40 / 2
+var z: Int = 20 + 200 * 2
--- test.asm
+++ test.asm
@@ -1,2 +1,14 @@
 {STORE, T$0, 420}
 {STORE, x, T$0}
+{STORE, T$1, 400}
+{STORE, T$2, 40}
+{STORE, T$3, 2}
+{DIV, T$4, T$2, T$3}
+{ADD, T$5, T$1, T$4}
+{STORE, y, T$5}
+{STORE, T$6, 20}
+{STORE, T$7, 200}
+{STORE, T$8, 2}
+{MUL, T$9, T$7, T$8}
+{ADD, T$10, T$6, T$9}
+{STORE, z, T$10}
