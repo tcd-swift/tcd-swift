32c80c4da37bac240a1f36df78e2a2af630658c4
Darragh Hickey <hickeyd1@tcd.ie>
2014-12-26 22:01:35 +0000

    Move Relations

--- src/backend/Allocate.cs
+++ src/backend/Allocate.cs
@@ -10,6 +10,7 @@ public class input{
 
     public static void Main(){
         registers = 12;
+
         List<string>[] input = new List<string>[10];
         input[0] = new List<string>(){"T","R1","R2","T"};
         input[1] = new List<string>(){"A","R2","A","T"};
@@ -21,10 +22,20 @@ public class input{
         input[7] = new List<string>(){"D","B","C","D","T"};
         input[8] = new List<string>(){"R1","R1","T"};
         input[9] = new List<string>(){"R3","R1","R3"};
+
+        string[,] moves = new string[6,2]{
+            {"T","R3"},
+            {"A","R1"},
+            {"B","R2"},
+            {"D","A"},
+            {"R1","C"},
+            {"R3","T"}
+        };
         
         nodes = new List<Node>();
 
         List<string> livein = new List<string>(){"R1","R2","R3"};
+
         Console.Write("\nLive In:");
         for(int i = 0; i < livein.Count; i++){
             Node n = getNode(livein[i]);
@@ -44,14 +55,20 @@ public class input{
             Console.WriteLine(output);
             
             Node n = getNode(line[0]);
-            Node t;
+            Node m;
             for(int j = 1; j < line.Count; j++){
-                t = getNode(line[j]);
-                n.link(t);
+                m = getNode(line[j]);
+                n.link(m);
             }
 
         }
-        
+
+        for(int i = 0; i < moves.GetLength(0); i++){
+            Node n = getNode(moves[i,0]);
+            Node m = getNode(moves[i,1]);
+            n.movelink(m);
+        }
+
         printGraph(nodes);
 
         List<string> results = assign(nodes, livein);
@@ -115,15 +132,17 @@ public class input{
 public class Node{
     public string id;
     public List<Node> interfere;
-    public List<string> move;
+    public List<Node> move;
     public bool isRegister;
+    public bool moveRelated;
 
     public Node(string ident){
         this.id = ident;
         this.interfere = new List<Node>();
-        this.move = new List<string>();
+        this.move = new List<Node>();
         Regex regex = new Regex(@"R\d?\d");
-        isRegister = regex.IsMatch(id); 
+        this.isRegister = regex.IsMatch(id);
+        this.moveRelated = false;
     }
 
     public string toString(){
@@ -132,6 +151,12 @@ public class Node{
         output = output + this.id + ":";
         for(int i = 0; i < this.interfere.Count; i++){
             output = output + " " + this.interfere[i].id;
+        }
+        if(this.moveRelated){
+            output = output + ", moveRelated:";
+            for(int i = 0; i < this.move.Count; i++){
+                output = output + " " + this.move[i].id;
+            }
         }
         output = output + ", isRegister: " + isRegister;
         return output;
@@ -150,6 +175,24 @@ public class Node{
         if(this.id != n.id && !this.contains(n.id)){    
             this.interfere.Add(n);
             n.interfere.Add(this);
+        }
+    }
+
+    public bool related(string s){
+        for(int i = 0; i < this.move.Count; i++){
+            if(this.move[i].id == s){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void movelink(Node n){
+        if(this.id != n.id && !this.related(n.id)){    
+            this.move.Add(n);
+            this.moveRelated = true;
+            n.move.Add(this);
+            n.moveRelated = true;
         }
     }
 }
