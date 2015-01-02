107eb63854a20c234889994d8200de922510f9b6
Darragh Hickey <hickeyd1@tcd.ie>
2015-01-02 02:27:00 +0000

    Changed Allocate to take in and give out list of tuples, much neater, at least in Main.cs

--- src/Main.cs
+++ src/Main.cs
@@ -20,27 +20,15 @@ class TCDSwift
                 parser.Write("output.ir");
             }
             IRStream tuples = parser.GetStream();
-            
-            IRGraph graph = new IRGraph(tuples);
-
-            List<string> livein;
-            List<List<string>> liveouts;
-            graph.ComputeLiveness(out livein, out liveouts);
 
             // Code Optimizations
             // SSA.DoSSAOptimizations(graph);
     
-            // Register Allocation
-            Dictionary<string,string> registerAllocation = Allocate.run(liveouts, livein);
-    
-            List<IRTuple> irstream_out = new List<IRTuple>(); 
-            foreach (IRTuple irt in tuples)
-            {
-                IRTuple translated = irt.TranslateNames(registerAllocation);
-                irstream_out.Add(translated);
-            }
+            // Register Allocation  
+            List<IRTuple> tuples_out = Allocate.run(tuples); 
+            
             // backenders should write ARM out to arg[1]
-            CodeGen.IRListToArmFile(irstream_out, arg[1]);
+            CodeGen.IRListToArmFile(tuples_out, arg[1]);
         }
         else Console.WriteLine("Usage: tcdscc <program.swift> <output.asm>");
     }
