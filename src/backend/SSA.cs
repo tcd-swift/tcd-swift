85203e199b0b81c1401afc7fcf36564bb740eddc
Kevin Baker <eire32kevin@gmail.com>
2015-01-08 18:27:59 +0000

    Cleaned up formatting in SSA class

--- src/backend/SSA.cs
+++ src/backend/SSA.cs
@@ -6,7 +6,8 @@ using Ident = System.String;
 
 public class SSA
 {
-  public static void DoSSAOptimizations(IRGraph graph) {
+  public static void DoSSAOptimizations(IRGraph graph)
+  {
     // convert into SSA form
     ConvertIntoSSAForm(graph);
 
@@ -21,30 +22,35 @@ public class SSA
   /*
    * Translate into SSA form
    */
-  private static void ConvertIntoSSAForm(IRGraph graph) {
+  private static void ConvertIntoSSAForm(IRGraph graph)
+  {
     DominatorTree dominatorTree = new DominatorTree(graph);
     InsertPhiFunctions(graph, dominatorTree);
     RenameVariables(graph, dominatorTree);
   }
 
-  private static void InsertPhiFunctions(IRGraph graph, DominatorTree dominatorTree) {
-    foreach (Ident v in graph.GetDefinedVars()) {
+  private static void InsertPhiFunctions(IRGraph graph, DominatorTree dominatorTree)
+  {
+    foreach (Ident v in graph.GetDefinedVars())
+    {
       // A(v) = blocks containing an assignment to v
       HashSet<IRBlock> Av = new HashSet<IRBlock>();
-      foreach (IRBlock block in graph.GetSetOfAllBlocks()) {
-        if (block.GetDefinedVars().Contains(v)) {
+      foreach (IRBlock block in graph.GetSetOfAllBlocks())
+      {
+        if (block.GetDefinedVars().Contains(v))
           Av.Add(block);
-        }
       }
 
       // place Phi tuple for each v in the iterated dominance frontier of A(v)
-      foreach (IRBlock Avblock in Av) {
-        foreach (IRBlock block in dominatorTree.GetDominanceFrontier(Avblock)) {
+      foreach (IRBlock Avblock in Av)
+      {
+        foreach (IRBlock block in dominatorTree.GetDominanceFrontier(Avblock))
+        {
           // Phi function should have as many arguments as it does predecessors
           List<Ident> sources = new List<Ident>();
-          foreach (IRBlock b in graph.GetPredecessors(block)) {
+          foreach (IRBlock b in graph.GetPredecessors(block))
             sources.Add(v);
-          }
+          
           IRTupleManyOp phi = new IRTupleManyOp(IrOp.PHI, v, sources);
 
           block.InsertStatement(phi, 0);
@@ -53,12 +59,14 @@ public class SSA
     }
   }
 
-  private static void RenameVariables(IRGraph graph, DominatorTree dominatorTree) {
+  private static void RenameVariables(IRGraph graph, DominatorTree dominatorTree)
+  {
     // setup data structures
     Dictionary<Ident, int> count = new Dictionary<Ident, int>();
     Dictionary<Ident, Stack<int>> stack = new Dictionary<Ident, Stack<int>>();
 
-    foreach (Ident ident in graph.GetDefinedVars()) {
+    foreach (Ident ident in graph.GetDefinedVars())
+    {
       count[ident] = 0;
       stack[ident] = new Stack<int>();
       stack[ident].Push(0);
@@ -68,9 +76,11 @@ public class SSA
     Search(graph.GetGraphHead(), dominatorTree, count, stack);
   }
 
-  private static void Search(IRBlock block, DominatorTree dominatorTree, Dictionary<Ident, int> count, Dictionary<Ident, Stack<int>> stack) {
+  private static void Search(IRBlock block, DominatorTree dominatorTree, Dictionary<Ident, int> count, Dictionary<Ident, Stack<int>> stack)
+  {
     // Algorithm from Ron Cytron et al to rename variables into SSA form
-    foreach (IRTuple irt in block.GetStatements()) {
+    foreach (IRTuple irt in block.GetStatements())
+    {
       // Console.Write("Type: " + irt.GetType() + " ");
       // irt.Print();
       // Console.WriteLine();
@@ -93,9 +103,12 @@ public class SSA
 
     // rename Phi function arguments in the successors
     List<IRBlock> successors = block.GetSuccessors();
-    for (int i = 0; i < successors.Count; i++) {
-      foreach (IRTuple stmt in block.GetStatements()) {
-        if (stmt.getOp() == IrOp.PHI) {
+    for (int i = 0; i < successors.Count; i++)
+    {
+      foreach (IRTuple stmt in block.GetStatements())
+      {
+        if (stmt.getOp() == IrOp.PHI)
+        {
           IRTupleManyOp phi = (IRTupleManyOp)stmt;
           Ident v = phi.GetSources()[i];
           int numbering = stack[v].Peek();
@@ -106,68 +119,74 @@ public class SSA
 
     // for each descendant do the Search(X) renaming process
     DominatorTreeNode node = dominatorTree.GetNode(block);
-    foreach (DominatorTreeNode descendant in node.GetDescendants()) {
+    foreach (DominatorTreeNode descendant in node.GetDescendants())
+    {
       Search(descendant.GetBlock(), dominatorTree, count, stack);
     }
 
     // pop stack phase
-    foreach (IRTuple irt in block.GetStatements()) { 
-      foreach (Ident v in irt.GetDefinedVars()) {
+    foreach (IRTuple irt in block.GetStatements())
+    { 
+      foreach (Ident v in irt.GetDefinedVars())
         stack[v].Pop();
-      }
     }
   }
 
-  private static void RenameTupleSourcesHelper(IRTuple tuple, Ident replaceIdent, int numbering) {
+  private static void RenameTupleSourcesHelper(IRTuple tuple, Ident replaceIdent, int numbering)
+  {
     Console.WriteLine();
     Console.Write("Before: ");
     tuple.Print();
     Console.WriteLine();
 
-    if (tuple.GetType() == typeof(IRTupleTwoOp)) {
+    if (tuple.GetType() == typeof(IRTupleTwoOp))
+    {
       IRTupleTwoOp twoTuple = (IRTupleTwoOp)tuple;
 
-      if (twoTuple.getSrc2() == replaceIdent) {
+      if (twoTuple.getSrc2() == replaceIdent)
         twoTuple.setSrc2(RenameVar(replaceIdent, numbering));
-      }
 
-      if (twoTuple.getSrc1() == replaceIdent) {
+      if (twoTuple.getSrc1() == replaceIdent)
         twoTuple.setSrc1(RenameVar(replaceIdent, numbering));
-      }
+
 
     } else if (tuple.GetType() == typeof(IRTupleOneOpIdent)) {
       IRTupleOneOpIdent oneTuple = (IRTupleOneOpIdent)tuple;
-      if (oneTuple.getSrc1() == replaceIdent) {
+      if (oneTuple.getSrc1() == replaceIdent)
         oneTuple.setSrc1(RenameVar(replaceIdent, numbering));
-      }
 
-    } else {
-      throw new Exception("You need to deal with more types: " + tuple.GetType());
-    }
+
+    } else throw new Exception("You need to deal with more types: " + tuple.GetType());
 
     Console.Write("After: ");
     tuple.Print();
     Console.WriteLine();
   }
 
-  private static Ident RenameVar(Ident old, int numbering) {
+  private static Ident RenameVar(Ident old, int numbering)
+  {
     return old + "_ssa" + numbering;
   }
 
   /*
    * Optimization Methods
    */
-  private static void DeadCodeElimination(IRGraph graph) {
+  private static void DeadCodeElimination(IRGraph graph)
+  {
     
   }
 
-  private static void ConstantPropagation(IRGraph graph) {
+  private static void ConstantPropagation(IRGraph graph)
+  {
+
   }
 
   /*
    * Translate out of SAA form
    */
-  private static void TranslateOutOfSSAForm(IRGraph graph) {
+  private static void TranslateOutOfSSAForm(IRGraph graph)
+  {
+
   }
 
 }
